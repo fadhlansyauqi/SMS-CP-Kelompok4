f3cac2f6e7c904962f0a6a5833f29705b13aab98
Awang Super <awangx008@gmail.com>
2023-12-15 21:40:03 +0700

    fix eror

--- app/Attendance.php
+++ app/Attendance.php
@@ -6,18 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attendance extends Model
 {
-<<<<<<< HEAD
-    
-    protected $fillable = [
-        'nis',
-        'nama',
-        'tanggal_masuk',
-        'jam_masuk',
-        'keterangan',
-    ];
-}
 
-=======
     protected $table = 'tbl_attendances';
 
     protected $fillable = [
@@ -33,4 +22,4 @@ class Attendance extends Model
         return $this->belongsTo('App\student', 'nis');
     }
 }
->>>>>>> 41cf50e572b840512ef5212777f256b79d51c5ff
+
