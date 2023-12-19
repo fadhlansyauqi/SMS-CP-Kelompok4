18f0cf9e45d617d6a5dc51b12793ffffd280eb95
wisnu0305 <wisnufebrian00@gmail.com>
2023-12-20 01:45:47 +0700

    tambah jadwal pelajaran

--- app/LessonHours.php
+++ app/LessonHours.php
@@ -9,4 +9,9 @@ class LessonHours extends Model
     protected $table = 'tbl_lesson_hours';
 
     protected $fillable = ['jam','waktu',];
+
+    public function classSchedule()
+    {
+        return $this->hasMany(ClassSchedule::class, 'id_lesson_hours', 'id');
+    }
 }
