3d3fed327b376bdfceca5f41a87acef655805a7e
fadhlansyauqi <adhansyauqi@gmail.com>
2023-12-19 13:48:44 +0700

    add relation from tbl_users to tbl_classes

--- database/migrations/2014_10_12_000000_create_tbl_users_table.php
+++ database/migrations/2014_10_12_000000_create_tbl_users_table.php
@@ -15,12 +15,11 @@ class CreateTblUsersTable extends Migration
     {
         Schema::create('tbl_users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('id_kelas')->unsigned()->nullable();
             $table->string('name');
             $table->enum('role', ['ADMIN', 'TEACHER', 'STUDENT'])->default('STUDENT');
             $table->string('email')->unique();
             $table->string('password');
-
-            $table->bigInteger('id_kelas');
             $table->rememberToken();
             $table->timestamps();
 
